9940b06034cf6765633a1cc974c706efaf921240
luizhnrs <luizhnrsg@gmail.com>
2024-08-02 22:33:05 -0300

    Views Implemented

--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from .models import User
+from .serializers import UserSerializer
+
+import json
+
+def databaseManagementInDjango():
+    data = User.objects.get(pk='Luiz')                  #OBJECT
+
+    data = User.objects.filter(user_age='23')           #QUERYSET
+
+    data = User.objects.exclude(user_age='25')          #QUERYSET
+
+    data.save()
+
+    data.delete()
